7af3c1aa4d88097929b6e9664ea7ba965ab0892c
Betide Studio <codingwithdevesh@gmail.com>
2024-09-22 21:08:50 +0530

    UpdateDeployment Function added

---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Deployments/EGIK_UpdateDeploymentProperties.cpp
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EGIK_UpdateDeploymentProperties.h"
+
+UEGIK_UpdateDeploymentProperties* UEGIK_UpdateDeploymentProperties::UpdateDeploymentProperties(
+	FEGIK_UpdateDeploymentPropertiesRequest Request)
+{
+	UEGIK_UpdateDeploymentProperties* Node = NewObject<UEGIK_UpdateDeploymentProperties>();
+	Node->Var_Request = Request;
+	return Node;
+}
+
+void UEGIK_UpdateDeploymentProperties::OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest,
+	TSharedPtr<IHttpResponse> HttpResponse, bool bArg)
+{
+	if (HttpResponse.IsValid())
+	{
+		if (EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+		{
+			OnSuccess.Broadcast(Var_Request.bIsJoinableBySession, FEGIK_ErrorStruct());
+		}
+		else
+		{
+			OnFailure.Broadcast(Var_Request.bIsJoinableBySession, FEGIK_ErrorStruct(HttpResponse->GetResponseCode(), "Failed to update deployment properties"));
+		}
+	}
+	else
+	{
+		OnFailure.Broadcast(Var_Request.bIsJoinableBySession, FEGIK_ErrorStruct(0, "Failed to deserialize response"));
+	}
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UEGIK_UpdateDeploymentProperties::Activate()
+{
+	Super::Activate();
+	FHttpModule* Http = &FHttpModule::Get();
+	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+	Request->SetVerb("PATCH");
+	Request->SetURL("https://api.edgegap.com/v1/deployments/" + Var_Request.RequestId);
+	Request->SetHeader("Content-Type", "application/json");
+	Request->SetHeader("Authorization", "token 3d6e71e0-5717-4e8b-959d-b374d004be73");
+	Request->OnProcessRequestComplete().BindUObject(this, &UEGIK_UpdateDeploymentProperties::OnResponseReceived);
+	if (!Request->ProcessRequest())
+	{
+		OnFailure.Broadcast(false, FEGIK_ErrorStruct(0, "Failed to process request"));
+		SetReadyToDestroy();
+		MarkAsGarbage();
+	}
+}
---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Deployments/EGIK_UpdateDeploymentProperties.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EGIKBlueprintFunctionLibrary.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "EGIK_UpdateDeploymentProperties.generated.h"
+
+USTRUCT(BlueprintType)
+struct FEGIK_UpdateDeploymentPropertiesRequest
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, Category = "Edgegap Integration Kit | Deployment")
+	FString RequestId;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Edgegap Integration Kit | Deployment")
+	bool bIsJoinableBySession;
+	
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FUpdateDeploymentPropertiesResponse, bool, bIsJoinableBySession, FEGIK_ErrorStruct, Error);
+UCLASS()
+class EDGEGAPINTEGRATIONKIT_API UEGIK_UpdateDeploymentProperties : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "Edgegap Integration Kit | Deployment")
+	static UEGIK_UpdateDeploymentProperties* UpdateDeploymentProperties(FEGIK_UpdateDeploymentPropertiesRequest Request);
+
+	void OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest, TSharedPtr<IHttpResponse> HttpResponse, bool bArg);
+	virtual void Activate() override;
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Deployment")
+	FUpdateDeploymentPropertiesResponse OnSuccess;
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Deployment")
+	FUpdateDeploymentPropertiesResponse OnFailure;
+
+private:
+	FEGIK_UpdateDeploymentPropertiesRequest Var_Request;
+};
