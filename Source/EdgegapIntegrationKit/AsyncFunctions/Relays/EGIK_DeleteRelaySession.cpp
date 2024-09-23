6d90d4b29d9ca8cd0a0be580907c63eb179464f1
Betide Studio <codingwithdevesh@gmail.com>
2024-09-23 09:20:50 +0530

    Delete Relay Session

---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Relays/EGIK_DeleteRelaySession.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EGIK_DeleteRelaySession.h"
+
+UEGIK_DeleteRelaySession* UEGIK_DeleteRelaySession::DeleteRelaySession(FString SessionId)
+{
+	UEGIK_DeleteRelaySession* Node = NewObject<UEGIK_DeleteRelaySession>();
+	Node->Var_SessionId = SessionId;
+	return Node;
+}
+
+void UEGIK_DeleteRelaySession::Activate()
+{
+	Super::Activate();
+	FHttpModule* Http = &FHttpModule::Get();
+	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+	Request->SetVerb("DELETE");
+	Request->SetURL("https://api.edgegap.com/v1/relays/sessions/" + Var_SessionId);
+	Request->SetHeader("Content-Type", "application/json");
+	Request->OnProcessRequestComplete().BindUObject(this, &UEGIK_DeleteRelaySession::OnResponseReceived);
+	if (!Request->ProcessRequest())
+	{
+		OnFailure.Broadcast(FEGIK_ErrorStruct(0, "Failed to process request"));
+		SetReadyToDestroy();
+		MarkAsGarbage();
+	}
+}
+
+void UEGIK_DeleteRelaySession::OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest,
+	TSharedPtr<IHttpResponse> HttpResponse, bool bArg)
+{
+	if (HttpResponse.IsValid())
+	{
+		if (EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+		{
+			OnSuccess.Broadcast(FEGIK_ErrorStruct());
+		}
+		else
+		{
+			OnFailure.Broadcast(FEGIK_ErrorStruct(HttpResponse->GetResponseCode(), HttpResponse->GetContentAsString()));
+		}
+	}
+	else
+	{
+		OnFailure.Broadcast(FEGIK_ErrorStruct(0, "Failed to process request"));
+	}
+}
---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Relays/EGIK_DeleteRelaySession.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EGIKBlueprintFunctionLibrary.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "EGIK_DeleteRelaySession.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnDeleteRelaySession, FEGIK_ErrorStruct, Error);
+UCLASS()
+class EDGEGAPINTEGRATIONKIT_API UEGIK_DeleteRelaySession : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Edgegap Integration Kit | Relay Sessions", meta = (BlueprintInternalUseOnly = "true"))
+	static UEGIK_DeleteRelaySession* DeleteRelaySession(FString SessionId);
+
+	virtual void Activate() override;
+	void OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest, TSharedPtr<IHttpResponse> HttpResponse, bool bArg);
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Relay Sessions")
+	FOnDeleteRelaySession OnSuccess;
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Relay Sessions")
+	FOnDeleteRelaySession OnFailure;
+
+private:
+	FString Var_SessionId;
+};
