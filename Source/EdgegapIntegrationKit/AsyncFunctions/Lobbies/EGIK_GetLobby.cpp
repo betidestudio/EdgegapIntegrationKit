72365dd3a5b7528f683a2039793b4e7685a466f1
Betide Studio <codingwithdevesh@gmail.com>
2024-09-23 09:20:29 +0530

    Get Lobby

---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Lobbies/EGIK_GetLobby.cpp
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EGIK_GetLobby.h"
+
+UEGIK_GetLobby* UEGIK_GetLobby::GetLobby(FString LobbyName)
+{
+	UEGIK_GetLobby* Node = NewObject<UEGIK_GetLobby>();
+	Node->Var_LobbyName = LobbyName;
+	return Node;
+}
+
+void UEGIK_GetLobby::Activate()
+{
+	Super::Activate();
+	FHttpModule* Http = &FHttpModule::Get();
+	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+	Request->SetVerb("GET");
+	Request->SetURL("https://api.edgegap.com/v1/lobbies/" + Var_LobbyName);
+	Request->SetHeader("Content-Type", "application/json");
+	Request->OnProcessRequestComplete().BindUObject(this, &UEGIK_GetLobby::OnResponseReceived);
+	if (!Request->ProcessRequest())
+	{
+		OnFailure.Broadcast(FEGIK_LobbyInfo(), FEGIK_ErrorStruct(0, "Failed to process request"));
+		SetReadyToDestroy();
+		MarkAsGarbage();
+	}
+}
+
+void UEGIK_GetLobby::OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest, TSharedPtr<IHttpResponse> HttpResponse,
+	bool bArg)
+{
+	if (HttpResponse.IsValid())
+	{
+		if (EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+		{
+			TSharedPtr<FJsonObject> JsonObject;
+			TSharedRef<TJsonReader<TCHAR>> Reader = TJsonReaderFactory<TCHAR>::Create(HttpResponse->GetContentAsString());
+			if (FJsonSerializer::Deserialize(Reader, JsonObject))
+			{
+				FEGIK_LobbyInfo Lobby;
+				Lobby.Name = JsonObject->GetStringField(TEXT("name"));
+				Lobby.Url = JsonObject->GetStringField(TEXT("url"));
+				Lobby.Status = JsonObject->GetStringField(TEXT("status"));
+				OnSuccess.Broadcast(Lobby, FEGIK_ErrorStruct());
+			}
+			else
+			{
+				OnFailure.Broadcast(FEGIK_LobbyInfo(), FEGIK_ErrorStruct(0, "Failed to deserialize response"));
+			}
+		}
+		else
+		{
+			OnFailure.Broadcast(FEGIK_LobbyInfo(), FEGIK_ErrorStruct(HttpResponse->GetResponseCode(), HttpResponse->GetContentAsString()));
+		}
+	}
+	else
+	{
+		OnFailure.Broadcast(FEGIK_LobbyInfo(), FEGIK_ErrorStruct(0, "Failed to deserialize response"));
+	}
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Lobbies/EGIK_GetLobby.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EGIKBlueprintFunctionLibrary.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "EGIK_GetLobby.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FEGIK_GetLobbyResponse, FEGIK_LobbyInfo, Result, FEGIK_ErrorStruct, Error);
+UCLASS()
+class EDGEGAPINTEGRATIONKIT_API UEGIK_GetLobby : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "Edgegap Integration Kit | Lobbies")
+	static UEGIK_GetLobby* GetLobby(FString LobbyName);
+
+	virtual void Activate() override;
+	void OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest, TSharedPtr<IHttpResponse> HttpResponse, bool bArg);
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Lobbies")
+	FEGIK_GetLobbyResponse OnSuccess;
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Lobbies")
+	FEGIK_GetLobbyResponse OnFailure;
+	
+private:
+	FString Var_LobbyName;
+};
