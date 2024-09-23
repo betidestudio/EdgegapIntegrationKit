992410f17332528532f588357e97056179125bbb
Betide Studio <codingwithdevesh@gmail.com>
2024-09-23 09:19:47 +0530

    List Relay Sessions

---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Relays/EGIK_ListRelaySessions.cpp
@@ -0,0 +1,71 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EGIK_ListRelaySessions.h"
+
+UEGIK_ListRelaySessions* UEGIK_ListRelaySessions::ListRelaySessions(int32 PageNumber)
+{
+	UEGIK_ListRelaySessions* Node = NewObject<UEGIK_ListRelaySessions>();
+	Node->Var_PageNumber = PageNumber;
+	return Node;
+}
+
+void UEGIK_ListRelaySessions::Activate()
+{
+	Super::Activate();
+	FHttpModule* Http = &FHttpModule::Get();
+	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+	Request->SetVerb("GET");
+	Request->SetURL("https://api.edgegap.com/v1/relays/sessions");
+	Request->SetHeader("Content-Type", "application/json");
+	Request->OnProcessRequestComplete().BindUObject(this, &UEGIK_ListRelaySessions::OnResponseReceived);
+	if (!Request->ProcessRequest())
+	{
+		OnFailure.Broadcast(FEGIK_ListRelaySessionsOutput(), FEGIK_ErrorStruct(0, "Failed to process request"));
+		SetReadyToDestroy();
+		MarkAsGarbage();
+	}
+}
+
+void UEGIK_ListRelaySessions::OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest,
+	TSharedPtr<IHttpResponse> HttpResponse, bool bArg)
+{
+	if (HttpResponse.IsValid())
+	{
+		if (EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+		{
+			TSharedPtr<FJsonObject> JsonObject;
+			TSharedRef<TJsonReader<TCHAR>> Reader = TJsonReaderFactory<TCHAR>::Create(HttpResponse->GetContentAsString());
+			if (FJsonSerializer::Deserialize(Reader, JsonObject))
+			{
+				FEGIK_ListRelaySessionsOutput Output;
+				Output.RelaySessions.Empty();
+				TArray<TSharedPtr<FJsonValue>> RelaySessionsArray = JsonObject->GetArrayField(TEXT("sessions"));
+				for(auto RelaySessionValue : RelaySessionsArray)
+				{
+					Output.RelaySessions.Add(RelaySessionValue->AsObject());
+				}
+				TSharedPtr<FJsonObject> PaginationObject = JsonObject->GetObjectField(TEXT("pagination"));
+				Output.Pagination.Number = PaginationObject->GetIntegerField(TEXT("number"));
+				Output.Pagination.NextPageNumber = PaginationObject->GetIntegerField(TEXT("next_page_number"));
+				Output.Pagination.PreviousPageNumber = PaginationObject->GetIntegerField(TEXT("previous_page_number"));
+				Output.Pagination.TotalPages = PaginationObject->GetObjectField(TEXT("paginator"))->GetIntegerField(TEXT("num_pages"));
+				Output.Pagination.bHasNextPage = PaginationObject->GetBoolField(TEXT("has_next"));
+				Output.Pagination.bHasPreviousPage = PaginationObject->GetBoolField(TEXT("has_previous"));
+				OnSuccess.Broadcast(Output, FEGIK_ErrorStruct());
+			}
+			else
+			{
+				OnFailure.Broadcast(FEGIK_ListRelaySessionsOutput(), FEGIK_ErrorStruct(0, "Failed to deserialize response"));
+			}
+		}
+		else
+		{
+			OnFailure.Broadcast(FEGIK_ListRelaySessionsOutput(), FEGIK_ErrorStruct(HttpResponse->GetResponseCode(), HttpResponse->GetContentAsString()));
+		}
+	}
+	else
+	{
+		OnFailure.Broadcast(FEGIK_ListRelaySessionsOutput(), FEGIK_ErrorStruct(0, "Failed to process request"));
+	}
+}
---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Relays/EGIK_ListRelaySessions.h
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EGIKBlueprintFunctionLibrary.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "EGIK_ListRelaySessions.generated.h"
+
+USTRUCT(BlueprintType)
+struct FEGIK_ListRelayPagination
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit")
+	int32 Number;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit")
+	int32 NextPageNumber;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit")
+	int32 PreviousPageNumber;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit")
+	int32 TotalPages;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit")
+	bool bHasNextPage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit")
+	bool bHasPreviousPage;
+};
+
+USTRUCT(BlueprintType)
+struct FEGIK_ListRelaySessionsOutput
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit")
+	TArray<FEGIK_RelaySessionInfo> RelaySessions;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit")
+	FEGIK_ListRelayPagination Pagination;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnListRelaySessions, FEGIK_ListRelaySessionsOutput, RelaySessions, FEGIK_ErrorStruct, Error);
+
+UCLASS()
+class EDGEGAPINTEGRATIONKIT_API UEGIK_ListRelaySessions : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Edgegap Integration Kit | Relay Sessions", meta = (BlueprintInternalUseOnly = "true"))
+	static UEGIK_ListRelaySessions* ListRelaySessions(int32 PageNumber);
+
+	virtual void Activate() override;
+	void OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest, TSharedPtr<IHttpResponse> HttpResponse, bool bArg);
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Relay Sessions")
+	FOnListRelaySessions OnSuccess;
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Relay Sessions")
+	FOnListRelaySessions OnFailure;
+
+private:
+	int32 Var_PageNumber;
+};
