6bb02026abfa3c92bc7000ad50ed170a6acaba96
Betide Studio <codingwithdevesh@gmail.com>
2024-09-23 09:22:14 +0530

    Authorize User On Relay Session

---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Relays/EGIK_AuthorizeUserOnRelaySession.cpp
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EGIK_AuthorizeUserOnRelaySession.h"
+
+UEGIK_AuthorizeUserOnRelaySession* UEGIK_AuthorizeUserOnRelaySession::AuthorizeUserOnRelaySession(
+	FEGIK_AuthorizeUserOnRelaySessionInput Input)
+{
+	UEGIK_AuthorizeUserOnRelaySession* Node = NewObject<UEGIK_AuthorizeUserOnRelaySession>();
+	Node->Var_Input = Input;
+	return Node;
+}
+
+void UEGIK_AuthorizeUserOnRelaySession::Activate()
+{
+	Super::Activate();
+	FHttpModule* Http = &FHttpModule::Get();
+	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+	Request->SetVerb("POST");
+	Request->SetURL("https://api.edgegap.com/v1/relays/sessions:authorize-user");
+	Request->SetHeader("Content-Type", "application/json");
+	TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject);
+	JsonObject->SetStringField("session_id", Var_Input.SessionId);
+	JsonObject->SetStringField("user_ip", Var_Input.UserIp);
+	FString Content;
+	TSharedRef<TJsonWriter<TCHAR>> Writer = TJsonWriterFactory<>::Create(&Content);
+	FJsonSerializer::Serialize(JsonObject.ToSharedRef(), Writer);
+	Request->SetContentAsString(Content);
+	Request->OnProcessRequestComplete().BindUObject(this, &UEGIK_AuthorizeUserOnRelaySession::OnResponseReceived);
+	if (!Request->ProcessRequest())
+	{
+		OnFailure.Broadcast(FEGIK_RelaySessionInfo(), FEGIK_ErrorStruct(0, "Failed to process request"));
+		SetReadyToDestroy();
+		MarkAsGarbage();
+	}
+}
+
+void UEGIK_AuthorizeUserOnRelaySession::OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest,
+	TSharedPtr<IHttpResponse> HttpResponse, bool bArg)
+{
+	if (HttpResponse.IsValid())
+	{
+		if (EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+		{
+			TSharedPtr<FJsonObject> JsonObject;
+			TSharedRef<TJsonReader<TCHAR>> Reader = TJsonReaderFactory<TCHAR>::Create(HttpResponse->GetContentAsString());
+			if (FJsonSerializer::Deserialize(Reader, JsonObject))
+			{
+				OnSuccess.Broadcast(JsonObject, FEGIK_ErrorStruct());
+			}
+			else
+			{
+				OnFailure.Broadcast(FEGIK_RelaySessionInfo(), FEGIK_ErrorStruct(0, "Failed to deserialize response"));
+			}
+		}
+		else
+		{
+			OnFailure.Broadcast(FEGIK_RelaySessionInfo(), FEGIK_ErrorStruct(HttpResponse->GetResponseCode(), HttpResponse->GetContentAsString()));
+		}
+	}
+	else
+	{
+		OnFailure.Broadcast(FEGIK_RelaySessionInfo(), FEGIK_ErrorStruct(0, "Failed to deserialize response"));
+	}
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Relays/EGIK_AuthorizeUserOnRelaySession.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EGIKBlueprintFunctionLibrary.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "EGIK_AuthorizeUserOnRelaySession.generated.h"
+
+USTRUCT(BlueprintType)
+struct FEGIK_AuthorizeUserOnRelaySessionInput
+{
+	GENERATED_BODY()
+
+	//Session Request ID (ex: 0724828881e2-S)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit")
+	FString SessionId;
+
+	//User Ip to Authorize
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit")
+	FString UserIp;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAuthorizeUserOnRelaySession, FEGIK_RelaySessionInfo, RelaySessionInfo, FEGIK_ErrorStruct, Error);
+UCLASS()
+class EDGEGAPINTEGRATIONKIT_API UEGIK_AuthorizeUserOnRelaySession : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Edgegap Integration Kit | Relay Sessions", meta = (BlueprintInternalUseOnly = "true"))
+	static UEGIK_AuthorizeUserOnRelaySession* AuthorizeUserOnRelaySession(FEGIK_AuthorizeUserOnRelaySessionInput Input);
+
+	virtual void Activate() override;
+	void OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest, TSharedPtr<IHttpResponse> HttpResponse, bool bArg);
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Relay Sessions")
+	FOnAuthorizeUserOnRelaySession OnSuccess;
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Relay Sessions")
+	FOnAuthorizeUserOnRelaySession OnFailure;
+
+private:
+	FEGIK_AuthorizeUserOnRelaySessionInput Var_Input;
+};
