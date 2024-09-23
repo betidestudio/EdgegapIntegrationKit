eefce64f686651637ca07f92efced7ba1b9cc83e
Betide Studio <codingwithdevesh@gmail.com>
2024-09-23 09:20:37 +0530

    Deploy Lobby

---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Lobbies/EGIK_DeployLobby.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EGIKBlueprintFunctionLibrary.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "EGIK_DeployLobby.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FDeployLobbyResponse, FEGIK_LobbyInfo, Result, FEGIK_ErrorStruct, Error);
+UCLASS()
+class EDGEGAPINTEGRATIONKIT_API UEGIK_DeployLobby : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "Edgegap Integration Kit | Lobbies")
+	static UEGIK_DeployLobby* DeployLobby(FString LobbyName);
+
+	virtual void Activate() override;
+	void OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest, TSharedPtr<IHttpResponse> HttpResponse, bool bArg);
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Lobbies")
+	FDeployLobbyResponse OnSuccess;
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Lobbies")
+	FDeployLobbyResponse OnFailure;
+
+private:
+	FString Var_LobbyName;
+};
