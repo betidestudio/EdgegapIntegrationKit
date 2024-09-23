5b4aad188069a2678a85c83f36e5df51aea7b881
Betide Studio <codingwithdevesh@gmail.com>
2024-09-23 09:19:33 +0530

    Remove Users from Relay Session

---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Relays/EGIK_RemoveUseronRelaySession.cpp
@@ -0,0 +1,69 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EGIK_RemoveUseronRelaySession.h"
+
+UEGIK_RemoveUseronRelaySession* UEGIK_RemoveUseronRelaySession::RemoveUseronRelaySession(FString SessionId,
+	FString AuthorizationToken)
+{
+	UEGIK_RemoveUseronRelaySession* Node = NewObject<UEGIK_RemoveUseronRelaySession>();
+	Node->Var_SessionId = SessionId;
+	Node->Var_AuthorizationToken = AuthorizationToken;
+	return Node;
+}
+
+void UEGIK_RemoveUseronRelaySession::Activate()
+{
+	Super::Activate();
+	FHttpModule* Http = &FHttpModule::Get();
+	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+	Request->SetVerb("POST");
+	Request->SetURL("https://api.edgegap.com/v1/relays/sessions:revoke-user");
+	Request->SetHeader("Content-Type", "application/json");
+	Request->SetHeader("Authorization", Var_AuthorizationToken);
+	TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject);
+	JsonObject->SetStringField("session_id", Var_SessionId);
+	JsonObject->SetStringField("authorization_token", Var_AuthorizationToken);
+	FString Content;
+	TSharedRef<TJsonWriter<TCHAR>> Writer = TJsonWriterFactory<>::Create(&Content);
+	FJsonSerializer::Serialize(JsonObject.ToSharedRef(), Writer);
+	Request->SetContentAsString(Content);
+	Request->OnProcessRequestComplete().BindUObject(this, &UEGIK_RemoveUseronRelaySession::OnResponseReceived);
+	if (!Request->ProcessRequest())
+	{
+		OnFailure.Broadcast(FEGIK_RelaySessionInfo(), FEGIK_ErrorStruct(0, "Failed to process request"));
+		SetReadyToDestroy();
+		MarkAsGarbage();
+	}
+}
+
+void UEGIK_RemoveUseronRelaySession::OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest,
+	TSharedPtr<IHttpResponse> HttpResponse, bool bArg)
+{
+	if (HttpResponse.IsValid())
+	{
+		if (EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+		{
+			TSharedPtr<FJsonObject> JsonObject;
+			TSharedRef<TJsonReader<TCHAR>> Reader = TJsonReaderFactory<TCHAR>::Create(HttpResponse->GetContentAsString());
+			if (FJsonSerializer::Deserialize(Reader, JsonObject))
+			{
+				OnSuccess.Broadcast(JsonObject, FEGIK_ErrorStruct());
+			}
+			else
+			{
+				OnFailure.Broadcast(FEGIK_RelaySessionInfo(), FEGIK_ErrorStruct(0, "Failed to deserialize response"));
+			}
+		}
+		else
+		{
+			OnFailure.Broadcast(FEGIK_RelaySessionInfo(), FEGIK_ErrorStruct(HttpResponse->GetResponseCode(), HttpResponse->GetContentAsString()));
+		}
+	}
+	else
+	{
+		OnFailure.Broadcast(FEGIK_RelaySessionInfo(), FEGIK_ErrorStruct(0, "Failed to process request"));
+	}
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Relays/EGIK_RemoveUseronRelaySession.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EGIKBlueprintFunctionLibrary.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "EGIK_RemoveUseronRelaySession.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnRemoveUseronRelaySession, FEGIK_RelaySessionInfo, RelaySessionInfo, FEGIK_ErrorStruct, Error);
+UCLASS()
+class EDGEGAPINTEGRATIONKIT_API UEGIK_RemoveUseronRelaySession : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Edgegap Integration Kit | Relay Sessions", meta = (BlueprintInternalUseOnly = "true"))
+	static UEGIK_RemoveUseronRelaySession* RemoveUseronRelaySession(FString SessionId, FString AuthorizationToken);
+
+	virtual void Activate() override;
+	void OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest, TSharedPtr<IHttpResponse> HttpResponse, bool bArg);
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Relay Sessions")
+	FOnRemoveUseronRelaySession OnSuccess;
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Relay Sessions")
+	FOnRemoveUseronRelaySession OnFailure;
+
+private:
+	FString Var_SessionId;
+	FString Var_AuthorizationToken;	
+};
