07a2ea7548e7627fdfbdb73d5f765b7bdfb3f0ca
Betide Studio <codingwithdevesh@gmail.com>
2024-09-23 09:19:56 +0530

    Get Relay Session

---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Relays/EGIK_GetRelaySession.cpp
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EGIK_GetRelaySession.h"
+
+UEGIK_GetRelaySession* UEGIK_GetRelaySession::GetRelaySession(FString SessionId)
+{
+	UEGIK_GetRelaySession* Node = NewObject<UEGIK_GetRelaySession>();
+	Node->Var_SessionId = SessionId;
+	return Node;
+}
+
+void UEGIK_GetRelaySession::Activate()
+{
+	Super::Activate();
+	FHttpModule* Http = &FHttpModule::Get();
+	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+	Request->SetVerb("GET");
+	Request->SetURL("https://api.edgegap.com/v1/relays/sessions/" + Var_SessionId);
+	Request->SetHeader("Content-Type", "application/json");
+	Request->OnProcessRequestComplete().BindUObject(this, &UEGIK_GetRelaySession::OnResponseReceived);
+	if (!Request->ProcessRequest())
+	{
+		OnFailure.Broadcast(FEGIK_RelaySessionInfo(), FEGIK_ErrorStruct(0, "Failed to process request"));
+		SetReadyToDestroy();
+		MarkAsGarbage();
+	}
+}
+
+void UEGIK_GetRelaySession::OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest,
+	TSharedPtr<IHttpResponse> HttpResponse, bool bArg)
+{
+	if (HttpResponse.IsValid())
+	{
+		if (EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+		{
+			TSharedPtr<FJsonObject> JsonObject;
+			TSharedRef<TJsonReader<TCHAR>> Reader = TJsonReaderFactory<TCHAR>::Create(HttpResponse->GetContentAsString());
+			if (FJsonSerializer::Deserialize(Reader, JsonObject))
+			{
+				OnSuccess.Broadcast(JsonObject, FEGIK_ErrorStruct());
+			}
+			else
+			{
+				OnFailure.Broadcast(FEGIK_RelaySessionInfo(), FEGIK_ErrorStruct(0, "Failed to deserialize response"));
+			}
+		}
+		else
+		{
+			OnFailure.Broadcast(FEGIK_RelaySessionInfo(), FEGIK_ErrorStruct(HttpResponse->GetResponseCode(), HttpResponse->GetContentAsString()));
+		}
+	}
+	else
+	{
+		OnFailure.Broadcast(FEGIK_RelaySessionInfo(), FEGIK_ErrorStruct(0, "Failed to deserialize response"));
+	}
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Relays/EGIK_GetRelaySession.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EGIKBlueprintFunctionLibrary.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "EGIK_GetRelaySession.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnGetRelaySession, FEGIK_RelaySessionInfo, RelaySessionInfo, FEGIK_ErrorStruct, Error);
+
+UCLASS()
+class EDGEGAPINTEGRATIONKIT_API UEGIK_GetRelaySession : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Edgegap Integration Kit | Relay Sessions", meta = (BlueprintInternalUseOnly = "true"))
+	static UEGIK_GetRelaySession* GetRelaySession(FString SessionId);
+
+	virtual void Activate() override;
+	void OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest, TSharedPtr<IHttpResponse> HttpResponse, bool bArg);
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Relay Sessions")
+	FOnGetRelaySession OnSuccess;
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Relay Sessions")
+	FOnGetRelaySession OnFailure;
+
+private:
+	FString Var_SessionId;
+};
