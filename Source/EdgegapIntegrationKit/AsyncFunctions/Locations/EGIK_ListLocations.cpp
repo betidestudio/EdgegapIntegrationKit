78adc5e9044a30544affd026fa3cdd5037b7c536
Betide Studio <codingwithdevesh@gmail.com>
2024-09-26 02:30:15 +0530

    List Locations Event Added

---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Locations/EGIK_ListLocations.cpp
@@ -0,0 +1,89 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+
+#include "EGIK_ListLocations.h"
+
+UEGIK_ListLocations* UEGIK_ListLocations::ListLocations(FEGIK_ListAllLocationsRequest ListLocationsRequest)
+{
+	UEGIK_ListLocations* BlueprintNode = NewObject<UEGIK_ListLocations>();
+	BlueprintNode->Var_ListLocationsRequest = ListLocationsRequest;
+	return BlueprintNode;
+}
+
+void UEGIK_ListLocations::OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest,
+	TSharedPtr<IHttpResponse> HttpResponse, bool bArg)
+{
+	TArray<FEGIK_AvailableLocationStruct> Locations;
+	if (HttpResponse.IsValid())
+	{
+		if (EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+		{
+			TSharedPtr<FJsonObject> JsonObject;
+			TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(HttpResponse->GetContentAsString());
+			if (FJsonSerializer::Deserialize(Reader, JsonObject))
+			{
+				TArray<TSharedPtr<FJsonValue>> LocationsArray = JsonObject->GetArrayField(TEXT("locations"));
+				for (TSharedPtr<FJsonValue> Value : LocationsArray)
+				{
+					TSharedPtr<FJsonObject> LocationObject = Value->AsObject();
+					FEGIK_AvailableLocationStruct Location;
+					Location.City = LocationObject->GetStringField(TEXT("city"));
+					Location.Country = LocationObject->GetStringField(TEXT("country"));
+					Location.Continent = LocationObject->GetStringField(TEXT("continent"));
+					Location.Timezone = LocationObject->GetStringField(TEXT("timezone"));
+					Location.AdministrativeDivision = LocationObject->GetStringField(TEXT("administrative_division"));
+					Location.Latitude = LocationObject->GetNumberField(TEXT("latitude"));
+					Location.Longitude = LocationObject->GetNumberField(TEXT("longitude"));
+					for(auto Tag : LocationObject->GetArrayField(TEXT("tags")))
+					{
+						Location.Tags.Add(Tag->AsObject()->GetStringField(TEXT("key")), Tag->AsObject()->GetStringField(TEXT("value")));
+					}
+					Locations.Add(Location);
+				}
+				OnSuccess.Broadcast(Locations, FEGIK_ErrorStruct());
+			}
+			else
+			{
+				OnFailure.Broadcast(TArray<FEGIK_AvailableLocationStruct>(), FEGIK_ErrorStruct(0, "Failed to parse JSON"));
+			}
+		}
+		else
+		{
+			OnFailure.Broadcast(TArray<FEGIK_AvailableLocationStruct>(), FEGIK_ErrorStruct(HttpResponse->GetResponseCode(), HttpResponse->GetContentAsString()));
+		}
+	}
+	else
+	{
+		OnFailure.Broadcast(TArray<FEGIK_AvailableLocationStruct>(), FEGIK_ErrorStruct(0, "Failed to process request"));
+	}
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UEGIK_ListLocations::Activate()
+{
+	Super::Activate();
+	FHttpModule* Http = &FHttpModule::Get();
+	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+	Request->SetVerb("GET");
+	Request->SetURL("https://api.edgegap.com/api/v1/locations");
+	Request->SetHeader("Content-Type", "application/json");
+	Request->SetHeader("Authorization", UEGIKBlueprintFunctionLibrary::GetAuthorizationKey());
+	TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject);
+	JsonObject->SetStringField("app", Var_ListLocationsRequest.App);
+	JsonObject->SetStringField("version", Var_ListLocationsRequest.Version);
+	JsonObject->SetStringField("type", Var_ListLocationsRequest.Type);
+	JsonObject->SetBoolField("tags", Var_ListLocationsRequest.bTags);
+	FString Content;
+	TSharedRef<TJsonWriter<TCHAR>> Writer = TJsonWriterFactory<>::Create(&Content);
+	FJsonSerializer::Serialize(JsonObject.ToSharedRef(), Writer);
+	Request->SetContentAsString(Content);
+	Request->OnProcessRequestComplete().BindUObject(this, &UEGIK_ListLocations::OnResponseReceived);
+	if (!Request->ProcessRequest())
+	{
+		OnFailure.Broadcast(TArray<FEGIK_AvailableLocationStruct>(), FEGIK_ErrorStruct(0, "Failed to process request"));
+		SetReadyToDestroy();
+		MarkAsGarbage();
+	}
+}
+
---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Locations/EGIK_ListLocations.h
@@ -0,0 +1,96 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HttpModule.h"
+#include "Interfaces/IHttpResponse.h"
+#include "EGIKBlueprintFunctionLibrary.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "EGIK_ListLocations.generated.h"
+
+USTRUCT(BlueprintType)
+struct FEGIK_ListAllLocationsRequest
+{
+	GENERATED_BODY()
+
+	//The App Name you want to filter with capacity
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	FString App;
+
+	//The Version Name you want to filter with capacity
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	FString Version;
+
+	//The type of the location
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	FString Type;
+
+	//Gets locations with tags. Set to: "true" to have the tags
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	bool bTags;
+};
+
+USTRUCT(BlueprintType)
+struct FEGIK_AvailableLocationStruct
+{
+	GENERATED_BODY()
+
+	//City Name
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	FString City;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	FString Country;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	FString Continent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	FString Timezone;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	FString AdministrativeDivision;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	float Latitude = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	float Longitude = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	FString Type;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	TMap<FString, FString> Tags;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FListLocationsResponse, const TArray<FEGIK_AvailableLocationStruct>&, Locations, const FEGIK_ErrorStruct&, Error);
+
+UCLASS()
+class EDGEGAPINTEGRATIONKIT_API UEGIK_ListLocations : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+
+	//List all the locations available to deploy on. You can specify an application and a version to filter out the locations that don’t have enough resources to deploy this application version.
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "Edgegap Integration Kit | Locations")
+	static UEGIK_ListLocations* ListLocations(FEGIK_ListAllLocationsRequest ListLocationsRequest);
+
+	void OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest, TSharedPtr<IHttpResponse> HttpResponse, bool bArg);
+	virtual void Activate() override;
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Locations")
+	FListLocationsResponse OnSuccess;
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Locations")
+	FListLocationsResponse OnFailure;
+
+private:
+	FEGIK_ListAllLocationsRequest Var_ListLocationsRequest;
+
+	
+
+	
+};
