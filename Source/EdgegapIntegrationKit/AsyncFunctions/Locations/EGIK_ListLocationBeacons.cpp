c539d2321ad51895fc0259d968413a6e6db7aeca
Betide Studio <codingwithdevesh@gmail.com>
2024-09-26 14:33:18 +0530

    List Location Beacons

---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Locations/EGIK_ListLocationBeacons.cpp
@@ -0,0 +1,76 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+#include "EGIK_ListLocationBeacons.h"
+
+UEGIK_ListLocationBeacons* UEGIK_ListLocationBeacons::ListLocationBeacons()
+{
+	UEGIK_ListLocationBeacons* BlueprintNode = NewObject<UEGIK_ListLocationBeacons>();
+	return BlueprintNode;
+}
+
+void UEGIK_ListLocationBeacons::OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest,
+	TSharedPtr<IHttpResponse> HttpResponse, bool bArg)
+{
+	TArray<FEGIK_ListLocationBeaconsResponse> LocationBeacons;
+	if (HttpResponse.IsValid())
+	{
+		if (EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+		{
+			TSharedPtr<FJsonObject> JsonObject;
+			TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(HttpResponse->GetContentAsString());
+			if (FJsonSerializer::Deserialize(Reader, JsonObject))
+			{
+				TArray<TSharedPtr<FJsonValue>> LocationBeaconsArray = JsonObject->GetArrayField(TEXT("locations"));
+				for (TSharedPtr<FJsonValue> Value : LocationBeaconsArray)
+				{
+					TSharedPtr<FJsonObject> LocationBeaconObject = Value->AsObject();
+					FEGIK_ListLocationBeaconsResponse LocationBeacon;
+					LocationBeacon.Host = LocationBeaconObject->GetStringField(TEXT("host"));
+					LocationBeacon.FQDN = LocationBeaconObject->GetStringField(TEXT("fqdn"));
+					LocationBeacon.UdpPort = LocationBeaconObject->GetNumberField(TEXT("udp_port"));
+					LocationBeacon.TcpPort = LocationBeaconObject->GetNumberField(TEXT("tcp_port"));
+					LocationBeacon.Location.City = LocationBeaconObject->GetObjectField(TEXT("location"))->GetStringField(TEXT("city"));
+					LocationBeacon.Location.Country = LocationBeaconObject->GetObjectField(TEXT("location"))->GetStringField(TEXT("country"));
+					LocationBeacon.Location.Continent = LocationBeaconObject->GetObjectField(TEXT("location"))->GetStringField(TEXT("continent"));
+					LocationBeacon.Location.Timezone = LocationBeaconObject->GetObjectField(TEXT("location"))->GetStringField(TEXT("timezone"));
+					LocationBeacon.Location.AdministrativeDivision = LocationBeaconObject->GetObjectField(TEXT("location"))->GetStringField(TEXT("administrative_division"));
+					LocationBeacon.LatitudeLongitude.Latitude = LocationBeaconObject->GetObjectField(TEXT("location"))->GetNumberField(TEXT("latitude"));
+					LocationBeacon.LatitudeLongitude.Longitude = LocationBeaconObject->GetObjectField(TEXT("location"))->GetNumberField(TEXT("longitude"));
+					LocationBeacons.Add(LocationBeacon);
+				}
+				OnSuccess.Broadcast(LocationBeacons, FEGIK_ErrorStruct());
+			}
+			else
+			{
+				OnFailure.Broadcast(TArray<FEGIK_ListLocationBeaconsResponse>(), FEGIK_ErrorStruct(0, "Failed to parse JSON"));
+			}
+		}
+		else
+		{
+			OnFailure.Broadcast(TArray<FEGIK_ListLocationBeaconsResponse>(), FEGIK_ErrorStruct(HttpResponse->GetResponseCode(), HttpResponse->GetContentAsString()));
+		}
+	}
+	else
+	{
+		OnFailure.Broadcast(TArray<FEGIK_ListLocationBeaconsResponse>(), FEGIK_ErrorStruct(0, "Failed to process request"));
+	}
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UEGIK_ListLocationBeacons::Activate()
+{
+	Super::Activate();
+	FHttpModule* Http = &FHttpModule::Get();
+	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+	Request->SetVerb("GET");
+	Request->SetURL("https://api.edgegap.com/v1/locations/beacons");
+	Request->SetHeader("Authorization", UEGIKBlueprintFunctionLibrary::GetAuthorizationKey());
+	Request->OnProcessRequestComplete().BindUObject(this, &UEGIK_ListLocationBeacons::OnResponseReceived);
+	if (!Request->ProcessRequest())
+	{
+		OnFailure.Broadcast(TArray<FEGIK_ListLocationBeaconsResponse>(), FEGIK_ErrorStruct(0, "Failed to process request"));
+		SetReadyToDestroy();
+		MarkAsGarbage();
+	}
+}
---
+++ Source/EdgegapIntegrationKit/AsyncFunctions/Locations/EGIK_ListLocationBeacons.h
@@ -0,0 +1,56 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HttpModule.h"
+#include "Interfaces/IHttpResponse.h"
+#include "EGIKBlueprintFunctionLibrary.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "EGIK_ListLocationBeacons.generated.h"
+
+USTRUCT(BlueprintType)
+struct FEGIK_ListLocationBeaconsResponse
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	FString Host;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	FString FQDN;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	int32 UdpPort;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	int32 TcpPort;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	FEGIK_LocationStruct Location;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Edgegap Integration Kit | Locations")
+	FEGIK_LatitudeLongitudeStruct LatitudeLongitude;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FListLocationBeaconsResponse, const TArray<FEGIK_ListLocationBeaconsResponse>&, Response, const FEGIK_ErrorStruct&, Error);
+
+UCLASS()
+class EDGEGAPINTEGRATIONKIT_API UEGIK_ListLocationBeacons : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "Edgegap Integration Kit | Locations")
+	static UEGIK_ListLocationBeacons* ListLocationBeacons();
+
+	void OnResponseReceived(TSharedPtr<IHttpRequest> HttpRequest, TSharedPtr<IHttpResponse> HttpResponse, bool bArg);
+	virtual void Activate() override;
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Locations")
+	FListLocationBeaconsResponse OnSuccess;
+
+	UPROPERTY(BlueprintAssignable, Category = "Edgegap Integration Kit | Locations")
+	FListLocationBeaconsResponse OnFailure;
+	
+};
